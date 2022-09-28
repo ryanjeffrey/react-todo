b8d6a3606a166543b16c9a5ed1eb2b644e7f2cfb
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-28 17:43:45 -0500

    Import components and create routes for each

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth/Auth';
+import Header from './components/Header/Header';
+import Items from './components/Items/Items';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="/items" component={Items} />
+        <Route exact path="/">
+          <Redirect to="/auth/sign-in" />
+        </Route>
+      </Switch>
     </div>
   );
 }
