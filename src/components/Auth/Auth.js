da700e7296c85bfbae272567593c1689e97df570
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-28 17:54:38 -0500

    Create basic Auth layout

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Auth.css';
+
 export default function Auth() {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-toggle">
+        <NavLink to="/auth/sign-in">Sign In</NavLink>
+        <NavLink to="/auth/sign-up">Sign Up</NavLink>
+      </div>
+      <div className="form-controls">
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="form-controls">
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button>Submit</button>
+    </div>
+  );
 }
