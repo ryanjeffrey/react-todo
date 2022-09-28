831972546b421dfd887f6b69781c858ebb2a8499
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-28 18:12:29 -0500

    If user is logged in, show custom greeting and allow sign out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,31 @@
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { signOut } from '../../services/auth';
+
 export default function Header() {
-  return <div>Header</div>;
+  const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+      setUser(null);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>To Do List</h1>
+      {user && (
+        <>
+          <div>hello {user.email}</div>
+          <button onClick={handleLogout}>
+            Sign Out
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
