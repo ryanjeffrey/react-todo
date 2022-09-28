ee28de32417f3d03a2fde00c374d31d68ae8713a
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-28 18:12:54 -0500

    If user is signed out, redirect to auth page

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,3 +1,11 @@
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+
 export default function Items() {
+  const { user } = useContext(UserContext);
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
   return <div>Items</div>;
 }
