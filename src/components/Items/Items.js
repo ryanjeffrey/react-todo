928766a6e8eb7aec9b3de03e5e36fc21a7157ac6
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-28 19:02:37 -0500

    Import useItems hook to display state changes for list of Items

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,11 +1,61 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
+import { useItems } from '../../hooks/useItems';
+import { createListItem, toggleListItem } from '../../services/items';
 
 export default function Items() {
+  const [description, setDescription] = useState('');
+  const { items, setItems } = useItems();
   const { user } = useContext(UserContext);
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
-  return <div>Items</div>;
+
+  const handleClick = async (item) => {
+    try {
+      const updatedItem = await toggleListItem(item);
+      setItems((prevItems) =>
+        prevItems.map((prevItem) => (prevItem.id === item.id ? updatedItem : prevItem))
+      );
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
+
+  const handleNewItem = async () => {
+    try {
+      await createListItem(description);
+      setItems((prev) => [...prev, { description }]);
+      setDescription('');
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
+
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>
+          <label className="checkbox">
+            <input type="checkbox" checked={item.complete} onChange={() => handleClick(item)} />
+            {item.description}
+          </label>
+        </div>
+      ))}
+      <div>
+        <input
+          type="text"
+          placeholder="new item"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={handleNewItem}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
 }
